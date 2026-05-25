8d981c6ef4dab8fcfde6358ec729eeed9be274a1
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2026-05-25 15:02:34 -0500

    Update to Gradle 9.5

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.42")
     annotationProcessor("org.projectlombok:lombok:1.18.42")
 
-    implementation("org.mapstruct:mapstruct:1.6.3")
+    compileOnly("org.mapstruct:mapstruct:1.6.3")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 
     testCompileOnly("org.projectlombok:lombok:1.18.42")
